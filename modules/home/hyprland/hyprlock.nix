ae1e390b237505c7a87d05359c8b426be0d9244f
leqndborgm <bothe.martin@gmx.de>
2025-05-31 19:33:32 +0200

    Updated face.jpg & removed old hyprlock options

--- modules/home/hyprland/hyprlock.nix
+++ modules/home/hyprland/hyprlock.nix
@@ -7,10 +7,8 @@
     enable = true;
     settings = {
       general = {
-        disable_loading_bar = true;
         grace = 10;
         hide_cursor = true;
-        no_fade_in = false;
       };
 
       background = [
