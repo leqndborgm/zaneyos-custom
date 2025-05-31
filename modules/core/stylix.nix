75989e5f64a5b300fbbc518d68524a40ff426c59
leqndborgm <bothe.martin@gmx.de>
2025-05-31 23:00:57 +0200

    Updated color scheme

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -10,49 +10,52 @@ in {
     enable = true;
     targets.plymouth.enable = false;
     image = stylixImage;
-    # base16Scheme = {
-    #   base00 = "282936";
-    #   base01 = "3a3c4e";
-    #   base02 = "4d4f68";
-    #   base03 = "626483";
-    #   base04 = "62d6e8";
-    #   base05 = "e9e9f4";
-    #   base06 = "f1f2f8";
-    #   base07 = "f7f7fb";
-    #   base08 = "ea51b2";
-    #   base09 = "b45bcf";
-    #   base0A = "00f769";
-    #   base0B = "ebff87";
-    #   base0C = "a1efe4";
-    #   base0D = "62d6e8";
-    #   base0E = "b45bcf";
-    #   base0F = "00f769";
-    # };
     polarity = "dark";
     opacity.terminal = 1.0;
+
+    base16Scheme = {
+      base00 = "0a0a10";
+      base01 = "151521";
+      base02 = "222233";
+      base03 = "33334d";
+      base04 = "5aa0af"; # dunkleres Cyan
+      base05 = "c8c8e0"; # helles, entsättigtes Grau
+      base06 = "8f6acb"; # gedeckteres Lila
+      base07 = "d0d0f0";
+      base08 = "d36fa3"; # dunkleres Pink
+      base09 = "c08050"; # gedecktes Orange
+      base0A = "c7c96d"; # gedecktes Gelbgrün
+      base0B = "64af6a"; # dunkles Grün
+      base0C = "5faebc"; # graublau
+      base0D = "505caa"; # dunkles Violettblau
+      base0E = "8f6acb"; # gedeckteres Lila
+      base0F = "b04c4c"; # dunkles Rot
+    };
+
     cursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Ice";
       size = 24;
     };
+
     fonts = {
       monospace = {
-        package = pkgs.nerd-fonts.jetbrains-mono;
-        name = "JetBrains Mono";
+        package = pkgs.nerd-fonts.caskaydia-cove;
+        name = "CaskaydiaCove Nerd Font";
       };
       sansSerif = {
-        package = pkgs.montserrat;
-        name = "Montserrat";
+        package = pkgs.nerd-fonts.fira-code;
+        name = "Fira Code Nerd Font";
       };
       serif = {
-        package = pkgs.montserrat;
-        name = "Montserrat";
+        package = pkgs.noto-fonts;
+        name = "Noto Serif";
       };
       sizes = {
-        applications = 12;
+        applications = 13;
         terminal = 15;
-        desktop = 11;
-        popups = 12;
+        desktop = 12;
+        popups = 13;
       };
     };
   };
