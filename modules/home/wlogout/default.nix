a1fab1d7b77a5750798dd5aa3af45e047d9a8cb4
leqndborgm <bothe.martin@gmx.de>
2025-05-31 18:24:21 +0200

    Edited wlogout + added icons

--- modules/home/wlogout/default.nix
+++ modules/home/wlogout/default.nix
@@ -14,6 +14,30 @@
         "text" = "Reboot";
         "keybind" = "r";
       }
+      {
+        "label" = "logout";
+        "action" = "sleep 1; hyprctl dispatch exit";
+        "text" = "Exit";
+        "keybind" = "e";
+      }
+      {
+        "label" = "suspend";
+        "action" = "sleep 1; systemctl suspend";
+        "text" = "Suspend";
+        "keybind" = "u";
+      }
+      {
+        "label" = "lock";
+        "action" = "sleep 1; hyprlock";
+        "text" = "Lock";
+        "keybind" = "l";
+      }
+      {
+        "label" = "hibernate";
+        "action" = "sleep 1; systemctl hibernate";
+        "text" = "Hibernate";
+        "keybind" = "h";
+      }
     ];
     style = ''
       * {
