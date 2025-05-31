0452ab08c7b4ee7ad4376ea8c8d1f53773bfa591
leqndborgm <bothe.martin@gmx.de>
2025-05-31 21:17:22 +0200

    updated style of hyprlock

--- modules/home/hyprland/hyprlock.nix
+++ modules/home/hyprland/hyprlock.nix
@@ -14,8 +14,8 @@
       background = [
         {
           path = "${config.home.homeDirectory}/Pictures/Wallpapers/caberpunk.jpg";
-          blur_passes = 2;
-          blur_size = 6;
+          blur_passes = 1;
+          blur_size = 4;
         }
       ];
 
@@ -24,9 +24,9 @@
           path = "${config.home.homeDirectory}/.config/face.jpg";
           size = 160;
           border_size = 4;
-          border_color = "rgba(110, 80, 180, 0.6)"; # dunkles Violett, leicht transparent
+          border_color = "rgba(110, 80, 180, 0.6)";
           rounding = -1;
-          position = "0, 0";
+          position = "0, 120";
           halign = "center";
           valign = "center";
         }
@@ -36,9 +36,9 @@
         {
           monitor = "";
           size = "250, 60";
-          position = "0, 120"; # direkt unter dem Profilbild
+          position = "0, -75";
           dots_center = true;
-          fade_on_empty = false; # ← zeigt sofort den Hinweistext!
+          fade_on_empty = false;
           placeholder_text = "Enter your passcode...";
           font_color = "rgba(220, 220, 255, 0.85)";
           inner_color = "rgba(130, 100, 200, 0.25)";
@@ -46,18 +46,40 @@
           outline_thickness = 5;
           rounding = 10;
           shadow_passes = 2;
+          fail_color = "rgba(117, 18, 28, 0.6)";
+          fail_text = "Wrong passcode...";
         }
       ];
 
       label = [
         {
-          text = "$USER";
+          text = "<< $USER >>";
           font_family = "JetBrainsMono Nerd Font";
           font_size = 16;
           color = "rgba(200, 200, 255, 0.8)";
-          position = "0, 80"; # ↑ jetzt weiter **über** dem Bild
+          position = "0, -15";
           halign = "center";
           valign = "center";
+        }
+
+        {
+          text = "$TIME";
+          font_family = "JetBrainsMono Nerd Font";
+          font_size = 20;
+          color = "rgba(220, 220, 255, 0.9)";
+          position = "0, -15";
+          halign = "center";
+          valign = "top";
+        }
+
+        {
+          text = "$LAYOUT";
+          font_family = "JetBrainsMono Nerd Font";
+          font_size = 20;
+          color = "rgba(220, 220, 225, 0.9)";
+          position = "0, 0";
+          halign = "center";
+          valign = "bottom";
         }
       ];
     };
