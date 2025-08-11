264e3b019689dd7ee8bb9d8b54e855db85e51a49
leqndborgm <bothe.martin@gmx.de>
2025-08-11 16:18:39 +0200

    Updated to newest version

--- modules/core/tuigreet.nix
+++ modules/core/tuigreet.nix
@@ -8,7 +8,7 @@
     settings = {
       default_session = {
         user = username;
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --greeting Wilkommen --remember --cmd Hyprland"; # start Hyprland with a TUI login manager
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --greeting Willkommen! --cmd Hyprland --remember"; # start Hyprland with a TUI login manager
       };
     };
   };
